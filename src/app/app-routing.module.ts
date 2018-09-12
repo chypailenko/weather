8aa552ad673738e54b0986c8c60779b9dc2a6ddf
chupailenko <p.chupailenko@selectoglobal.com>
2018-09-12 10:29:12 +0300

    added home page

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,14 @@
+import {NgModule} from '@angular/core';
+import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
+import {HomeComponent} from './containers/home/home.component';
+
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(appRoutes, {preloadingStrategy: PreloadAllModules})],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {SharedModule} from './shared/shared.module';
+import {CoreModule} from './core/core.module';
+import {ContainersModule} from './containers/containers.module';
+import {ComponentsModule} from './components/components.module';
+import {AppRoutingModule} from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    SharedModule,
+    CoreModule,
+    ContainersModule,
+    ComponentsModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
